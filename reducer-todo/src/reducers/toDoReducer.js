e06527ebeb5987e750721844b6b710837d3e4e43
bea03 <nicole.beamer@icloud.com>
2019-09-11 14:26:07 -0600

    step one done

--- reducer-todo/src/reducers/toDoReducer.js
+++ reducer-todo/src/reducers/toDoReducer.js
@@ -1,3 +1,27 @@
-export const initialState = [
-    
-]
+export const initialState = {
+
+    todos: [ 
+        {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 0
+      },
+
+      {
+        item: 'Learn about Context API',
+        completed: false,
+        id: 1
+      },
+
+      {
+        item: 'Learn about Redux',
+        completed: false,
+        id: 2
+      },
+    ]
+};
+
+export const reducer = (state, action) => {
+    return state;
+  }
+  
